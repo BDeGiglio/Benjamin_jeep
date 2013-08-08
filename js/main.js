7bad7f7280282dac0a024afb859d9041bb9dc5e6
BenDeGiglio <bdegiglio@gmail.com>
2013-08-08 18:02:22 -0400

    added color picker for jeep

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 
+window.addEventListener("load", init);
+
+function init() {
+
+	$('#colorizer-black').click(function() {
+		$('.jeepImage').attr('id','jeepColorBlack');
+	});
+	
+	$('#colorizer-green').click(function(){
+		$('.jeepImage').attr('id','jeepColorGreen');
+	});
+	$('#colorizer-white').click(function(){
+		$('.jeepImage').attr('id','jeepColorWhite');
+	});
+	$('#colorizer-silver').click(function(){
+		$('.jeepImage').attr('id','jeepColorSilver');
+	});
+	$('#colorizer-blue').click(function(){
+		$('.jeepImage').attr('id','jeepColorBlue');
+	});
+	$('#colorizer-tan').click(function(){
+		$('.jeepImage').attr('id','jeepColorTan');
+	});
+	$('#colorizer-red').click(function(){
+		$('.jeepImage').attr('id','jeepColorRed');
+	});
+	$('#colorizer-b`rown').click(function(){
+		$('.jeepImage').attr('id','jeepColorBrown');
+	});
+
+}
